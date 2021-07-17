4f5a5ea65c0db3685aa015ed6e73c1d5529b2641
tortilla <tortilla1310@gmail.com>
2021-07-17 22:05:42 +0200

    Homework for Lesson1

---
+++  homework1-3.py
@@ -0,0 +1,14 @@
+# Задание №3. По введенным пользователем координатам двух точек вывести уравнение прямой вида y=kx+b, проходящей через эти точки.
+print('Coordinates of A(x1; y1):')
+x1 = float(input('x1 = '))
+y1 = float(input('y1 = '))
+
+print('Coordinates of B(x2; y2):')
+x2 = float(input('x2 = '))
+y2 = float(input('y2 = '))
+
+
+k = (y1 - y2) / (x1 - x2)
+b = y2 -k * x2
+print('y = %.2f*x + %.2f' % (k, b))
+    
